61f484ad72eebbd567ef096478c50936e6abd39c
Rocky Xu <wesharer@gmail.com>
2014-04-04 11:56:37 +0800

    add DNT.Diag.Data.LiveDataItemComparer

---
+++ DNT/Diag/Data/LiveDataItemComparer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DNT.Diag.Data
+{
+	public class LiveDataItemComparer : IComparer<LiveDataItem>
+	{
+		public int Compare(LiveDataItem x, LiveDataItem y)
+		{
+			if (x == null)
+				throw new ArgumentNullException ("x");
+			if (y == null)
+				throw new ArgumentNullException ("y");
+
+			return x.IndexForSort.CompareTo (y.IndexForSort);
+		}
+	}
+}
+
