2d311095acef4496a8511bdf4b30959e17bae043
Rocky Xu <wesharer@gmail.com>
2014-04-04 11:55:33 +0800

    add DNT.Diag.Data.LiveDataBuffer

---
+++ DNT/Diag/Data/LiveDataBuffer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace DNT.Diag.Data
+{
+	public class LiveDataBuffer
+	{
+		private byte[] buff;
+		private int length;
+
+		public LiveDataBuffer ()
+		{
+			buff = new byte[32];
+			length = 0;
+		}
+
+		public byte[] Buff
+		{
+			get {
+				lock (this) {
+					return buff;
+				}
+			}
+		}
+
+		public int Length
+		{
+			get {
+				lock (this) {
+					return length;
+				}
+			}
+			private set { length = value; }
+		}
+
+		public void CopyTo(byte[] buff, int offset, int length)
+		{
+			lock (this) {
+				if (this.buff.Length < length) {
+					this.buff = new byte[length];
+				}
+				Array.Copy (buff, offset, this.buff, 0, length);
+				Length = length;
+			}
+		}
+
+		public byte this[int index]
+		{
+			get {
+				lock (this) {
+					return buff [index];
+				}
+			}
+		}
+	}
+}
+
