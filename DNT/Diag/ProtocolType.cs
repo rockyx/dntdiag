a1c85d2fcdc0e15360ac0d4d2b4708262a0b0c4a
Rocky Xu <wesharer@gmail.com>
2014-04-04 21:25:24 +0800

    Add ProtocolType enum

---
+++ DNT/Diag/ProtocolType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DNT.Diag
+{
+	public enum ProtocolType
+	{
+		J1850VPW,
+		//
+		J1805PWM,
+		//
+		ISO9141_2,
+		//
+		ISO14230,
+		//
+		ISO15765,
+		//
+		ALDL160,
+		//
+		ALDL8192,
+		//
+		KWP1281,
+		//
+		MikuniECU200,
+		//
+		MikuniECU300,
+		//
+		BesturnKWP
+		//
+	}
+}
+
