17decd464b5883c95b3d48033d4967f998df7d63
Rocky Xu <wesharer@gmail.com>
2014-04-04 11:48:12 +0800

    add DNT.Diag.Settings

--- DNT/Diag/Settings.cs
+++ DNT/Diag/Settings.cs
@@ -4,8 +4,25 @@ namespace DNT.Diag
 {
 	public static class Settings
 	{
-		public Settings ()
+		private static Language lang;
+		private static string langText;
+		public static Language Language
 		{
+			get { return lang; }
+			set {
+				lang = value;
+				langText = ToString (lang);
+			}
+		}
+
+		public static string ToString(Language value)
+		{
+			return value.ToString ().Replace ('_', '-');
+		}
+
+		public static string LanguageText
+		{
+			get { return langText; }
 		}
 	}
 }
