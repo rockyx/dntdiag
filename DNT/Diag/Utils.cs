8a7392125f71818b82d8b5ed787eac6ee4b86050
Rocky Xu <wesharer@gmail.com>
2014-04-04 16:40:19 +0800

    What?

--- DNT/Diag/Utils.cs
+++ DNT/Diag/Utils.cs
@@ -2,10 +2,30 @@
 
 namespace DNT.Diag.Android
 {
-	public class Utils
+	public static class Utils
 	{
-		public Utils ()
+		public static byte LoByte<T> (T value)
 		{
+			ushort v = Convert.ToUInt16 (value);
+			return (byte)(v & 0x00FF);
+		}
+
+		public static byte HiByte<T> (T value)
+		{
+			ushort v = Convert.ToUInt16 (value);
+			return (byte)((v & 0xFF00) >> 8);
+		}
+
+		public static ushort LoWord<T> (T value)
+		{
+			uint v = Convert.ToUInt32 (value);
+			return (ushort)(v & 0x0000FFFF);
+		}
+
+		public static ushort HiWord<T> (T value)
+		{
+			uint v = Convert.ToUInt32 (value);
+			return (ushort)((v & 0xFFFF0000) >> 16);
 		}
 	}
 }
