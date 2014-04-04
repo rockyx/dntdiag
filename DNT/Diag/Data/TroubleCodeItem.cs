2c3cf6decd2f22c850ac3b7412cfc2fe0edc9d4f
Rocky Xu <wesharer@gmail.com>
2014-04-04 11:57:32 +0800

    add DNT.Diag.Data.TroubleCodeItem

---
+++ DNT/Diag/Data/TroubleCodeItem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DNT.Diag.Data
+{
+	public class TroubleCodeItem
+	{
+		private string code;
+		private string content;
+		private string description;
+
+		public TroubleCodeItem(string code,
+			string content,
+			string description)
+		{
+			this.code = code;
+			this.content = content;
+			this.description = description;
+		}
+
+		public TroubleCodeItem ()
+			: this("", "", "")
+		{
+
+		}
+
+		public string Code
+		{
+			get { return code; }
+			set { code = value; }
+		}
+
+		public string Content
+		{
+			get { return content; }
+			set { content = value; }
+		}
+
+		public string Description
+		{
+			get { return description; }
+			set { description = value; }
+		}
+	}
+}
+
